cc0295617d7b1b183eee48e0f14e8991c574c9c1
boone5 <boonehunter03@gmail.com>
2023-06-16 21:57:03 -0500

    Initial Commit

---
+++ Fetch_TakeHomeProject/Fetch_TakeHomeProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_TakeHomeProjectApp.swift
+//  Fetch_TakeHomeProject
+//
+//  Created by Boone on 6/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_TakeHomeProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
